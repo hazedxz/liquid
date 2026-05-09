a83369cfd0fa7018f97b8f3a7505fc9249105f5b
hazedxz <tumblr1275@gmail.com>
2026-05-09 15:12:14 -0400

    Refactor StartMenu.h for improved readability

--- StartMenu.h
+++ StartMenu.h
@@ -16,7 +16,7 @@ LRESULT CALLBACK StartMenuProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPara
             FillRect(hdc, &ps.rcPaint, bgBrush); DeleteObject(bgBrush);
             SetBkMode(hdc, TRANSPARENT);
             
-            // Barra de busqueda
+            
             HBRUSH searchBrush = CreateSolidBrush(RGB(45, 45, 50));
             SelectObject(hdc, searchBrush); SelectObject(hdc, GetStockObject(NULL_PEN));
             RoundRect(hdc, 30, 30, 420, 65, 10, 10);
@@ -25,22 +25,22 @@ LRESULT CALLBACK StartMenuProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPara
             TextOutA(hdc, 45, 38, "Buscar aplicaciones, configuraciones y documentos", 49);
             DeleteObject(searchBrush);
 
-            // Titulo "Anclado"
+            
             HFONT titleFont = CreateFontA(16, 0, 0, 0, FW_BOLD, FALSE, FALSE, FALSE, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, CLEARTYPE_QUALITY, VARIABLE_PITCH, "Segoe UI");
             SelectObject(hdc, titleFont); SetTextColor(hdc, RGB(255, 255, 255));
             TextOutA(hdc, 40, 95, "Anclado", 7);
 
-            // Iconos Funcionales
+            
             HICON hEdge = LoadIcon(NULL, IDI_APPLICATION); 
             DrawIconEx(hdc, 60, 140, hEdge, 32, 32, 0, NULL, DI_NORMAL); TextOutA(hdc, 50, 180, "Navegador", 9);
             DrawIconEx(hdc, 210, 140, hEdge, 32, 32, 0, NULL, DI_NORMAL); TextOutA(hdc, 205, 180, "Archivos", 8);
             DrawIconEx(hdc, 360, 140, hEdge, 32, 32, 0, NULL, DI_NORMAL); TextOutA(hdc, 355, 180, "Terminal", 8);
 
-            // Linea separadora inferior
+            
             HPEN linePen = CreatePen(PS_SOLID, 1, RGB(60, 60, 65)); SelectObject(hdc, linePen);
             MoveToEx(hdc, 0, 480, NULL); LineTo(hdc, 450, 480); DeleteObject(linePen);
 
-            // Botones de Energia
+            
             SetTextColor(hdc, RGB(255, 255, 255)); TextOutA(hdc, 40, 505, "Usuario", 7);
             
             HBRUSH pwrBrush = CreateSolidBrush(RGB(255, 60, 60)); SelectObject(hdc, pwrBrush);
@@ -57,7 +57,7 @@ LRESULT CALLBACK StartMenuProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPara
         }
         case WM_LBUTTONDOWN: {
             int x = LOWORD(lParam); int y = HIWORD(lParam);
-            // Zonas clickeables
+            
             if (x >= 40 && x <= 100 && y >= 130 && y <= 190) ShellExecuteA(NULL, "open", "msedge.exe", NULL, NULL, SW_SHOW);
             if (x >= 190 && x <= 250 && y >= 130 && y <= 190) ShellExecuteA(NULL, "open", "explorer.exe", NULL, NULL, SW_SHOW);
             if (x >= 340 && x <= 400 && y >= 130 && y <= 190) ShellExecuteA(NULL, "open", "cmd.exe", NULL, NULL, SW_SHOW);
@@ -77,7 +77,7 @@ void ToggleStartMenu() {
         WNDCLASSA wc = {0}; wc.lpfnWndProc = StartMenuProc; wc.hInstance = GetModuleHandle(NULL); wc.lpszClassName = "StartMenuClass";
         RegisterClassA(&wc);
         int sw = GetSystemMetrics(SM_CXSCREEN); int sh = GetSystemMetrics(SM_CYSCREEN);
-        int startX = (sw - 450) / 2; // Centrado estilo Win11
+        int startX = (sw - 450) / 2; 
         hwndStartMenu = CreateWindowExA(WS_EX_TOPMOST | WS_EX_TOOLWINDOW | WS_EX_LAYERED,
             "StartMenuClass", "StartMenu", WS_POPUP, startX, sh - 550 - 80, 450, 550, NULL, NULL, GetModuleHandle(NULL), NULL);
         SetLayeredWindowAttributes(hwndStartMenu, 0, 240, LWA_ALPHA);
